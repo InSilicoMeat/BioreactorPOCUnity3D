a8b6712a316665eafe76bbbdf6b601cee4f75c5a
Simon Kahan <simon.kahan@gmail.com>
2020-06-06 08:09:24 -0700

    accommodate internal data files; added simple interactive camera control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,22 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 public class CameraController : MonoBehaviour
 {
-    public GameObject gobject;
-    public float CameraSpeed = .1f;
-    private Vector3 offset; Quaternion rot_offset;
-    // Start is called before the first frame update
-    void Start()
+
+    /*
+    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+    Converted to C# 27-02-13 - no credit wanted.
+    Simple flycam I made, since I couldn't find any others made public.  
+    Made simple to use (drag and drop, done) for regular keyboard layout  
+    wasd : basic movement
+    shift : Makes camera accelerate
+    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+
+
+    float mainSpeed = 100.0f; //regular speed
+    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 1000.0f; //Maximum speed when holdin gshift
+    float camSens = .1f; //How sensitive it with mouse
+    private Vector3 freeMouse; //the origin for angle displacement when shift is pressed
+    private float totalRun = 1.0f;
+
+    private void Start()
     {
-        offset = transform.position - new Vector3(0, 0, 0);
-        rot_offset = transform.rotation * gobject.transform.rotation;
+        freeMouse = new Vector3(0, 0, 0); //arbitrary
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            Vector3 diffMouse = Input.mousePosition - freeMouse;
+            Debug.Log(Input.mousePosition);
+            diffMouse = new Vector3(-diffMouse.y * camSens, diffMouse.x * camSens, 0);
+            diffMouse = new Vector3(transform.eulerAngles.x + diffMouse.x, transform.eulerAngles.y + diffMouse.y, 0);
+
+            transform.eulerAngles = diffMouse;
+            //Mouse  camera angle done.
+        }
+
+        //Keyboard commands
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift) ||Input.GetKey(KeyCode.RightShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+        if (Input.GetKey(KeyCode.Space))
+        { //If player wants to move on X and Z axis only
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        {
+            transform.Translate(p);
+        }
+
+        freeMouse = Input.mousePosition;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        //transform.position = gobject.transform.position + offset;
-        //transform.rotation = gobject.transform.rotation * rot_offset;
-        transform.position += new Vector3(CameraSpeed, 0, 0);
-
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        return p_Velocity;
     }
 }
--- Assets/Scripts/DataReader.cs
+++ Assets/Scripts/DataReader.cs
@@ -31,26 +31,56 @@ using System.Text.RegularExpressions;
 public class DataReader : MonoBehaviour
 {
     public GameObject Cell, Microcarrier;
-    public string DataPath;
     public float FrameRate = 10f; //frames per second
+
+    public bool data_files_are_external = false; //are data files in Resources?
+    public string DataPath; //set from unity editor when files are external
+    TextAsset[] _all_files;
 
     float _timer; //time since last change in seconds
     int _frame_number; //ranges from 0 to number of pvtp files at DataPath
     int _num_frames;
-    string[] _pvtp_files;
+
+
+    string[] _pvtp_files; //used when data files are external
+
+    TextAsset[] _pvtp_assets; //used when data files are internal
+    Dictionary<string, TextAsset> _textAssetD;
+
     Dictionary<Int32, GameObject> _objects = new Dictionary<Int32, GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("In Start");
         _frame_number = 0;
         _timer = 0.0f;
         //read in first pvtp file
-        _pvtp_files = System.IO.Directory.GetFiles(DataPath, "*.pvtp");
-        _num_frames = _pvtp_files.Length;
+        if (data_files_are_external)
+        {
+            DataPath = "/Users/simonkahan/CMMC POC/Assets/Resources/output60b/";
+            _pvtp_files = Directory.GetFiles(DataPath, "*.pvtp.bytes", SearchOption.TopDirectoryOnly);
+            _num_frames = _pvtp_files.Length;
+        }
+        else
+        { //gotta be a simpler way than this...
+            DataPath = Application.dataPath + "/Resources/" + "output60b" + "/";
+            _all_files = Resources.LoadAll<TextAsset>("output60b");
+            _textAssetD = new Dictionary<string, TextAsset>();
+            List<TextAsset> _pvtp_file_list = new List<TextAsset>();
+            foreach (TextAsset t in _all_files)
+            {
+                if (t.name.Contains(".pvtp")) _pvtp_file_list.Add(t);
+                _textAssetD.Add(t.name, t);
+            }
+            _pvtp_assets = _pvtp_file_list.ToArray();
+            _num_frames = _pvtp_assets.Length;
+        }
         Debug.Log("Found " + _num_frames + " pvtp files\n");
-        ProcessNextFile();
+        if (data_files_are_external)
+        {
+            ProcessNextFile();
+        }
+        else ProcessNextAsset();
     }
 
     // Update is called once per frame
@@ -67,7 +97,145 @@ public class DataReader : MonoBehaviour
                 foreach (GameObject go in _objects.Values) Destroy(go);
                 _objects.Clear();
             }
-            ProcessNextFile();
+            if (data_files_are_external)
+            {
+                ProcessNextFile();
+            }
+            else ProcessNextAsset();
+        }
+    }
+
+    void ProcessNextAsset()
+    {
+        if (_num_frames < 1) return;
+        Debug.Log("Processing file " + _pvtp_assets[_frame_number].name + " for frame " + _frame_number + "\n");
+
+        string line;
+        int color_offset = 0, radius_offset = 0, stress_offset = 0, id_offset = 0, point_offset = 0;
+        List<TextAsset> vtp_assets;
+
+        vtp_assets = new List<TextAsset>();
+        string s = _pvtp_assets[_frame_number].text;
+
+        string[] lines = Regex.Split(s, "\n|\r|\r\n");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i].Contains("Source"))
+            {
+                Match new_file = Regex.Match(lines[i], "\"([^\"]*)\"");
+                string new_file_name = new_file.Value.Replace("\"", "");
+                TextAsset val;
+                var res = _textAssetD.TryGetValue(new_file_name, out val);
+
+                if (!res) Debug.Log("Source file " + new_file_name + " specified in " + _pvtp_assets[_frame_number].name + " not found.");
+                vtp_assets.Add(val);
+            }
+        }
+        // here is where we'd like to "unload" the pvtp file resource from memory, though it's small
+        int num_vtp_files = vtp_assets.Count;
+
+        Debug.Log("Found " + num_vtp_files + " vtp files\n");
+
+        foreach (TextAsset vtp_asset in vtp_assets)
+        {
+            Debug.Log("Processing " + vtp_asset.name);
+
+            int file_posn = 0;
+            int num_points = 0;
+            s = System.Text.Encoding.Default.GetString(vtp_asset.bytes);
+            //s = vtp_asset.text; //doesn't work
+            Debug.Log("Examining " + vtp_asset.name + ": has " + s.Length + " chars");
+            const string regex = "\"([^\"]*)\""; //quoted items
+            lines = Regex.Split(s, "\n|\r|\r\n");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                line = lines[i];
+                file_posn += line.Length + 1;
+                if (line.Contains("NumberOfPoints"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[0].Value.Replace("\"", ""));
+                    num_points = Int32.Parse(co[0].Value.Replace("\"", ""));
+                }
+                else if (line.Contains("color"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[3].Value.Replace("\"", ""));
+                    color_offset = Int32.Parse(co[3].Value.Replace("\"", ""));
+                }
+                else if (line.Contains("radius"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[3].Value.Replace("\"", ""));
+                    radius_offset = Int32.Parse(co[3].Value.Replace("\"", ""));
+                }
+                else if (line.Contains("stress"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[3].Value.Replace("\"", ""));
+                    stress_offset = Int32.Parse(co[3].Value.Replace("\"", ""));
+                }
+                else if (line.Contains("id"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[3].Value.Replace("\"", ""));
+                    id_offset = Int32.Parse(co[3].Value.Replace("\"", ""));
+                }
+                else if (line.Contains("NumberOfComponents"))
+                {
+                    MatchCollection co = Regex.Matches(line, regex);
+                    //Debug.Log(line + ":" + co[3].Value.Replace("\"", ""));
+                    point_offset = Int32.Parse(co[3].Value.Replace("\"", ""));
+                    break;
+                }
+            }
+
+            while (file_posn < s.Length && (s[file_posn] != '_')) file_posn++;
+            if (file_posn == s.Length) Debug.Log("Reached end of vtp file unexpectedly");
+            file_posn++;
+
+            byte[] b = vtp_asset.bytes;
+
+            Debug.Log("b has length " + b.Length);
+            if (Convert.ToChar(b[file_posn - 1]) != '_') Debug.Log("DataReader misalignment at " + file_posn);
+            
+            // We have reached the binary data, since it always begins just after the
+            //   first _ following the header info.
+
+            int c_file_posn = file_posn + 4 + color_offset;
+            int r_file_posn = file_posn + 4 + radius_offset;
+            int i_file_posn = file_posn + 4 + id_offset;
+            int p_file_posn = file_posn + 4 + point_offset;
+
+            // An assumption is that an object whose id is less than the number of
+            //   agents already added to the scene must already be in the scene.
+
+            for (int i = 0; i < num_points; i++)
+            {
+                int id = Convert.ToInt32(BitConverter.ToDouble(b, i_file_posn)); i_file_posn += 8;
+                double z = BitConverter.ToDouble(b, p_file_posn) / 1000 - 27.5; p_file_posn += 8;
+                double x = BitConverter.ToDouble(b, p_file_posn) / 1000 - 27.5; p_file_posn += 8;
+                double y = BitConverter.ToDouble(b, p_file_posn) / 1000; p_file_posn += 8;
+                Vector3 p = new Vector3(Convert.ToSingle(x), Convert.ToSingle(y), Convert.ToSingle(z));
+                float r = Convert.ToSingle(BitConverter.ToDouble(b, r_file_posn)) * 2 / 1000; r_file_posn += 8;
+                int c = Convert.ToInt32(BitConverter.ToDouble(b, c_file_posn)); c_file_posn += 8;
+
+                if (_objects.TryGetValue(id, out GameObject obj))
+                {
+                    //can we compare type to expected type? if originally a cell is this still a cell?
+                    obj.transform.position = p;
+                    obj.transform.localScale = new Vector3(r, r, r);
+                }
+                else
+                {
+                    //Debug.Log("Add " + (c==1? "cell":"microcarrier") + " at " + p);
+                    if (c == 1) obj = Instantiate(Cell, p, Quaternion.identity);
+                    else obj = Instantiate(Microcarrier, p, Quaternion.identity);
+                    obj.transform.localScale = new Vector3(r, r, r);
+                    _objects.Add(id, obj);
+                }
+            }
+            // unload the assets
         }
     }
 
@@ -78,21 +246,21 @@ public class DataReader : MonoBehaviour
 
         string line;
         int color_offset = 0, radius_offset = 0, stress_offset = 0, id_offset = 0, point_offset = 0;
-        List<string> vtp_files = new List<string>();
+        List<string> vtp_files;
 
+        vtp_files = new List<string>();
+        System.IO.StreamReader file = new System.IO.StreamReader(_pvtp_files[_frame_number]);
+        while ((line = file.ReadLine()) != null)
         {
-            System.IO.StreamReader file = new System.IO.StreamReader(_pvtp_files[_frame_number]);
-            while ((line = file.ReadLine()) != null)
+            if (line.Contains("Source"))
             {
-                if (line.Contains("Source"))
-                {
-                    Match new_file = Regex.Match(line, "\"([^\"]*)\"");
-                    vtp_files.Add(new_file.Value.Replace("\"", ""));
-                }
+                Match new_file = Regex.Match(line, "\"([^\"]*)\"");
+                vtp_files.Add(new_file.Value.Replace("\"", "") + ".bytes");
             }
-            file.Close();
         }
-        Debug.Log("Found " + vtp_files.Count + " vtp files\n");
+        file.Close();
+        int num_vtp_files = vtp_files.Count;
+        Debug.Log("Found " + num_vtp_files + " vtp files\n");
 
         foreach (string fname in vtp_files)
         {
@@ -100,7 +268,7 @@ public class DataReader : MonoBehaviour
 
             int file_posn = 0;
             int num_points = 0;
-            System.IO.StreamReader file = new System.IO.StreamReader(DataPath + fname);
+            file = new System.IO.StreamReader(DataPath + fname);
             const string regex = "\"([^\"]*)\""; //quoted items
             while ((line = file.ReadLine()) != null)
             {
@@ -190,7 +358,7 @@ public class DataReader : MonoBehaviour
                 double x = pfile.ReadDouble() / 1000 - 27.5; //  and adjust origin
                 double y = pfile.ReadDouble() / 1000;
                 Vector3 p = new Vector3(Convert.ToSingle(x), Convert.ToSingle(y), Convert.ToSingle(z));
-                float r = Convert.ToSingle(rfile.ReadDouble()) / 1000;
+                float r = Convert.ToSingle(rfile.ReadDouble()) * 2 / 1000;
                 int c = Convert.ToInt32(cfile.ReadDouble());
 
                 if (_objects.TryGetValue(id, out GameObject obj))
@@ -198,13 +366,13 @@ public class DataReader : MonoBehaviour
                     //sfile.BaseStream.Seek(8, SeekOrigin.Current);
                     //can we compare type to expected type? if originally a cell is this still a cell?
                     obj.transform.position = p;
-                    obj.transform.localScale = new Vector3(2 * r, 2 * r, 2 * r);
+                    obj.transform.localScale = new Vector3(r, r, r);
                 }
                 else
                 {
                     if (c == 1) obj = Instantiate(Cell, p, Quaternion.identity);
                     else obj = Instantiate(Microcarrier, p, Quaternion.identity);
-                    obj.transform.localScale = new Vector3(2 * r, 2 * r, 2 * r);
+                    obj.transform.localScale = new Vector3(r, r, r);
                     _objects.Add(id, obj);
                 }
             }
