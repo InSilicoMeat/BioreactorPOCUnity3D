274588117e028506e99b30e3db6e458ed06a1f9d
Simon Kahan <simon.kahan@gmail.com>
2020-07-13 08:43:57 -0700

    updated scene to include text scripts

--- Assets/Content.cs
+++ Assets/Content.cs
@@ -7,10 +7,11 @@ public class Content : MonoBehaviour
 {
     string _menu_text = "MENU: <esc> toggles\nFOCUS: f advances\nUNFOCUS: u\nFORWARD: {w, W}\nBACKWARD: {s, S}\nLEFT: {a, A}\nRIGHT: {d, D}\nROTATE: <ctrl> mouse\nSPEED: +/-\nPAUSE: <spc> toggles\nHOME: h";
     bool _short = true;
+    GameObject _go;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _go = gameObject;
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class Content : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             _short = _short ? false : true;
-            if (_short) gameObject.GetComponent<TextMeshProUGUI>().text = "MENU: <esc> toggles";
+            if (_short) _go.GetComponent<TextMeshProUGUI>().text = "MENU: <esc> toggles";
             else gameObject.GetComponent<TextMeshProUGUI>().text = _menu_text;
         }
 
